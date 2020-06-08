ff1e799a2376af203365a16ecac0b4695a8e9f81
Sofia <anasofia.costa90@gmail.com>
2020-06-08 11:26:49 +0100

    beer, brewingkit, order and snack models

---
+++ server/models/brewingkit.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  name: { type: String },
+  photo: { type: String },
+  price: {
+    amount: { type: Number },
+    currency: {
+      type: String
+    }
+  },
+  description: { type: String },
+  id: { type: String }
+});
+
+const Model = mongoose.model('BrewingKit', schema);
+
+module.exports = Model;
