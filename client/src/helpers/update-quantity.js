68aff4e5eadd1b905da4661fb841ed5b9154bc83
kkbrains1 <karen.hersschens@gmail.com>
2020-06-11 10:57:25 +0100

    client - small update to error page

--- client/src/helpers/update-quantity.js
+++ client/src/helpers/update-quantity.js
@@ -1,7 +1,7 @@
 const calcQuantity = (shoppingBasket, product) => {
   const productInShoppingBasket = shoppingBasket.find(item => item.product._id === product._id);
   //console.log('calcQuantity', 'shoppingBasket', shoppingBasket, 'product', product);
-  //console.log('productInShoppingBasket', productInShoppingBasket);
+  console.log('productInShoppingBasket', productInShoppingBasket);
   let quantity = 0;
   //console.log('quantity b4 if', quantity);
   if (productInShoppingBasket) {
