97ab9c9233430b3bfb39b5308fb2da0891e3478f
Vasco Matos <vascomato@gmail.com>
2020-06-12 09:41:50 +0100

    bug fix2

---
+++ server/seed/seed.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const URI = process.env.MONGODB_URI;
+
+const Products = require('../models/product');
+
+const PRODUCT_DATA = require('./products2.json');
+
+const run = async () => {
+  try {
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true
+    });
+    const products = await Products.create(PRODUCT_DATA);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+run();
